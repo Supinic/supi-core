4dd7d48ffbd4d2a12a007bd24a053d83cb261736
Supinic <supinic@protonmail.com>
2020-04-18 19:57:54 +0200

    extended APIError, started using it in command execution

--- objects/errors/api.js
+++ objects/errors/api.js
@@ -1,6 +1,11 @@
 module.exports = class APIError extends sb.Error {
-	constructor (...args) {
-		super(...args);
+	constructor (object) {
+		super(object);
+
+		this.statusCode = object.statusCode ?? null;
+		this.reason = object.reason ?? null;
+		this.apiName = object.apiName ?? null;
+
 		this.type = "API";
 	}
 
