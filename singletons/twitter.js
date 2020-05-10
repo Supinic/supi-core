984aece867c3facd119d0dacb87e1486c02a4314
Supinic <supinic@protonmail.com>
2020-05-10 17:23:17 +0200

    finally got rid of the over-bloated and deprecated twitter module
    now using twitter-lite
    now loading client and module on-demand only
    made the fetch method a lot easier to configure - using object for options

--- singletons/twitter.js
+++ singletons/twitter.js
@@ -10,6 +10,9 @@ module.exports = (function (Module) {
 	"use strict";
 
 	return class Twitter extends Module {
+		#client = null;
+		#module = null;
+
 		static singleton () {
 			if (!Twitter.module) {
 				const missingConfigs = mandatoryConfigs.filter(key => !sb.Config.has(key));
@@ -24,72 +27,64 @@ module.exports = (function (Module) {
 			return Twitter.module;
 		}
 
-		static userParams (user, max) {
-			return {
-				screen_name: user,
-				count: max,
-				exclude_replies: true,
-				include_rts: false
-			};
-		}
-
-		constructor () {
-			super();
-
-			const TwitterClient = require("twitter");
-			this.client = new TwitterClient({
-				consumer_key: sb.Config.get("TWITTER_CONSUMER_KEY"),
-				consumer_secret: sb.Config.get("TWITTER_CONSUMER_SECRET"),
-				access_token_key: sb.Config.get("TWITTER_ACCESS_TOKEN_KEY"),
-				access_token_secret: sb.Config.get("TWITTER_ACCESS_TOKEN_SECRET")
-			});
-		}
-
-		lastUserTweets (user) {
-			return new Promise((resolve, reject) => {
-				this.client.get("statuses/user_timeline", Twitter.userParams(user, 1), (err, resp) => {
-					if (err) {
-						resolve({
-							success: false,
-							text: "Twitter account '" + user + "' not found, or it is protected."
-						});
-					}
-					else {
-						if (resp && Array.isArray(resp) && resp.length > 0) {
-							resolve({
-								success: true,
-								date: new sb.Date(resp[0].created_at).valueOf(),
-								text: resp[0].text
-							});
-						}
-						else {
-							resolve ({
-								success: false,
-								text: "That twitter account was not found, or it is protected."
-							});
-						}
-					}
+		/**
+		 * Fetches user tweets based on provided options.
+		 * @param {Object} options
+		 * @param {string} [options.username] The username as it appears in the @username Twitter syntax.
+		 * @param {number} [options.userID] Internal Twitter user ID.
+		 * @param {number} [options.count=1] If set, this is the limit of provided tweets in the reply.
+		 * @param {boolean} [options.includeReplies=false] Whether or not to include tweet replies.
+		 * @param {boolean} [options.includeRetweets=false] Whether or not to include native retweets.
+		 * @param {boolean} [options.includeUser=false] Whether or not to include the user object in each tweet.
+		 * @returns {Promise<Object>}
+		 */
+		fetchTweets (options = {}) {
+			if (!options.username && !options.userID) {
+				throw new sb.Error({
+					message: "No twitter user identifier provided (ID or name)"
 				});
+			}
+			else if (options.username && options.userID) {
+				throw new sb.Error({
+					message: "Both types of twitter use identifiers provided"
+				});
+			}
+
+			return this.client.get("statuses/user_timeline", {
+				screen_name: options.username ?? null,
+				user_id: options.userID ?? null,
+				count: options.count ?? 1,
+				trim_user: !options.includeUser,
+				exclude_replies: !options.includeReplies,
+				include_rts: !options.includeRetweets
 			});
 		}
 
-		raw (request, url, params) {
-			return new Promise((resolve, reject) => {
-				this.client[request](url, params, (err, resp) => {
-					if (err) {
-						reject(err);
-					}
-					else {
-						resolve(resp);
-					}
+		get client () {
+			if (!this.#client) {
+				this.#client = new this.module({
+					consumer_key: sb.Config.get("TWITTER_CONSUMER_KEY"),
+					consumer_secret: sb.Config.get("TWITTER_CONSUMER_SECRET"),
+					access_token_key: sb.Config.get("TWITTER_ACCESS_TOKEN_KEY"),
+					access_token_secret: sb.Config.get("TWITTER_ACCESS_TOKEN_SECRET")
 				});
-			});
+			}
+
+			return this.#client;
+		}
+
+		get module () {
+			if (!this.#module) {
+				this.#module = require("twitter-lite");
+			}
+
+			return this.#module;
 		}
 
 		get modulePath () { return "twitter"; }
 
 		destroy () {
-			this.client = null;
+			this.#client = null;
 		}
 	};
 });
