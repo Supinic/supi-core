507ab0223827e7ff40408559c1a9c6c95c09b586
Supinic <supinic@protonmail.com>
2020-11-20 18:15:38 +0100

    removed stack property - not really used anyway

--- objects/errors/generic-request.js
+++ objects/errors/generic-request.js
@@ -6,7 +6,6 @@ module.exports = class GenericRequestError extends sb.Error {
 		this.statusMessage = object.statusMessage ?? null;
 		this.message = object.message ?? null;
 		this.hostname = object.hostname ?? null;
-		this.stack = object.stack ?? null;
 	}
 
 	static get name () { return "GenericRequestError"; }
