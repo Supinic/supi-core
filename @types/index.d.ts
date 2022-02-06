827d18a34b9f57e87d2696a619d13f9857094f23
Supinic <supinic@protonmail.com>
2022-02-07 00:23:26 +0100

    type files for remaining `objects` classes

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,26 +1,61 @@
+// objects
+import { CustomDate as Date } from "./objects/date";
+import { CustomError as Error, errors } from "./objects/error";
+import { CustomPromise as Promise } from "./objects/promise";
+import { URLParams } from "./objects/url-params";
+
+// classes
 import { AwayFromKeyboard } from "./classes/afk";
 import { Banphrase } from "./classes/banphrase";
 import { Channel } from "./classes/channel";
 import { ChatModule } from "./classes/chat-module";
 import { Command } from "./classes/command";
+import { Config } from "./classes/config";
 import { Filter } from "./classes/filter";
 import { GotProxy as Got } from "./classes/got";
 import { Platform } from "./classes/platform";
-import { User } from "./classes/user";
+import { Reminder } from "./classes/reminder";
+import { User } from "./classes/user"
 
-import { CustomDate as Date } from "./objects/date";
+// singletons
+import { CacheSingleton as Cache } from "./singletons/cache";
+import { CooldownManagerSingleton as CooldownManager } from "./singletons/cooldown-manager";
+import { LocalRequestSingleton as LocalRequest } from "./singletons/local-request";
+import { LoggerSingleton as Logger } from "./singletons/logger";
+import { PastebinSingleton as Pastebin } from "./singletons/pastebin";
+import { SandboxSingleton as Sandbox } from "./singletons/sandbox";
+import { UtilsSingleton as Utils } from "./singletons/utils";
+import { VLCSingleton as VideoLanConnector } from "./singletons/vlc-connector";
+import { QuerySingleton as Query } from "./singletons/query";
 
 export declare namespace sb {
     export {
+        Date,
+        Error,
+        errors,
+        Promise,
+        URLParams,
+
         AwayFromKeyboard,
         Banphrase,
         Channel,
         ChatModule,
         Command,
-        Date,
+        Config,
         Filter,
         Got,
+        Reminder,
         Platform,
-        User
+        User,
+
+        Cache,
+        CooldownManager,
+        LocalRequest,
+        Logger,
+        Pastebin,
+        Sandbox,
+        Utils,
+        VideoLanConnector,
+        Query
     };
 }
