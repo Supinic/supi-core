06dc8fda51849d7a07a79f0fc36c2cc8de13297f
Supinic <supinic@protonmail.com>
2022-03-15 19:39:09 +0100

    updated top-level typescript declaration

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -28,74 +28,78 @@ import { SandboxSingleton } from "./singletons/sandbox";
 import { UtilsSingleton } from "./singletons/utils";
 import { VLCSingleton } from "./singletons/vlc-connector";
 
-export declare type GlobalSbObject = {
-    Date: typeof CustomDate,
-    Error: typeof CustomError,
-    errors,
-    Promise: typeof CustomPromise,
-    URLParams: URLParams,
+declare type GlobalSbObject = {
+	Date: typeof CustomDate,
+	Error: typeof CustomError,
+	errors: typeof errors,
+	Promise: typeof CustomPromise,
+	URLParams: URLParams,
 
-    AwayFromKeyboard: AwayFromKeyboard,
-    Banphrase: Banphrase,
-    Channel: Channel,
-    ChatModule: ChatModule,
-    Command: Command,
-    Config: Config,
-    Filter: Filter,
-    Got: typeof Got,
-    Reminder: Reminder,
-    Platform: Platform,
-    User: User,
+	AwayFromKeyboard: AwayFromKeyboard,
+	Banphrase: Banphrase,
+	Channel: Channel,
+	ChatModule: ChatModule,
+	Command: Command,
+	Config: Config,
+	Filter: Filter,
+	Got: typeof Got,
+	Reminder: Reminder,
+	Platform: Platform,
+	User: User,
 
-    Cache: InstanceType<typeof CacheSingleton>,
-    CooldownManager: InstanceType<typeof CooldownManagerSingleton>,
-    LocalRequest: InstanceType<typeof LocalRequestSingleton>,
-    Logger: InstanceType<typeof LoggerSingleton>,
-    Pastebin: InstanceType<typeof QuerySingleton>,
-    Query: InstanceType<typeof QuerySingleton>,
-    Sandbox: InstanceType<typeof SandboxSingleton>,
-    Utils: InstanceType<typeof UtilsSingleton>,
-    VideoLANConnector: InstanceType<typeof VLCSingleton>
+	Cache: InstanceType<typeof CacheSingleton>,
+	CooldownManager: InstanceType<typeof CooldownManagerSingleton>,
+	LocalRequest: InstanceType<typeof LocalRequestSingleton>,
+	Logger: InstanceType<typeof LoggerSingleton>,
+	Pastebin: InstanceType<typeof PastebinSingleton>,
+	Query: InstanceType<typeof QuerySingleton>,
+	Sandbox: InstanceType<typeof SandboxSingleton>,
+	Utils: InstanceType<typeof UtilsSingleton>,
+	VideoLANConnector: InstanceType<typeof VLCSingleton>
 };
 
-export declare namespace sb {
-    const Cache: InstanceType<typeof CacheSingleton>;
-    const CooldownManager: InstanceType<typeof CooldownManagerSingleton>;
-    const LocalRequest: InstanceType<typeof LocalRequestSingleton>;
-    const Logger: InstanceType<typeof LoggerSingleton>;
-    const Pastebin: InstanceType<typeof PastebinSingleton>;
-    const Query: InstanceType<typeof QuerySingleton>;
-    const Sandbox: InstanceType<typeof SandboxSingleton>;
-    const Utils: InstanceType<typeof UtilsSingleton>;
-    const VideoLANConnector: InstanceType<typeof VLCSingleton>;
+declare function initialize (): Promise<GlobalSbObject>;
 
-    export {
-        CustomDate,
-        CustomError,
-        errors,
-        CustomPromise,
-        URLParams,
+export = initialize;
 
-        AwayFromKeyboard,
-        Banphrase,
-        Channel,
-        ChatModule,
-        Command,
-        Config,
-        Filter,
-        Got,
-        Reminder,
-        Platform,
-        User,
-
-        Cache,
-        CooldownManager,
-        LocalRequest,
-        Logger,
-        Pastebin,
-        Query,
-        Sandbox,
-        Utils,
-        VideoLANConnector
-    };
-}
+// export declare namespace sb {
+//     const Cache: InstanceType<typeof CacheSingleton>;
+//     const CooldownManager: InstanceType<typeof CooldownManagerSingleton>;
+//     const LocalRequest: InstanceType<typeof LocalRequestSingleton>;
+//     const Logger: InstanceType<typeof LoggerSingleton>;
+//     const Pastebin: InstanceType<typeof PastebinSingleton>;
+//     const Query: InstanceType<typeof QuerySingleton>;
+//     const Sandbox: InstanceType<typeof SandboxSingleton>;
+//     const Utils: InstanceType<typeof UtilsSingleton>;
+//     const VideoLANConnector: InstanceType<typeof VLCSingleton>;
+//
+//     export {
+//         CustomDate,
+//         CustomError,
+//         errors,
+//         CustomPromise,
+//         URLParams,
+//
+//         AwayFromKeyboard,
+//         Banphrase,
+//         Channel,
+//         ChatModule,
+//         Command,
+//         Config,
+//         Filter,
+//         Got,
+//         Reminder,
+//         Platform,
+//         User,
+//
+//         Cache,
+//         CooldownManager,
+//         LocalRequest,
+//         Logger,
+//         Pastebin,
+//         Query,
+//         Sandbox,
+//         Utils,
+//         VideoLANConnector
+//     };
+// }
