7885061591589af2c6d758367a3d2800bd987966
Supinic <supinic@protonmail.com>
2022-09-13 18:32:01 +0200

    moved all JSDoc definitions into the appropriate `.d.ts` file

--- @types/objects/date.d.ts
+++ @types/objects/date.d.ts
@@ -1,5 +1,6 @@
 // import { Date as NativeDate } from "lib.es6";
 
+export declare type DayOfTheWeek = "Sunday" | "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday";
 export declare type Month = "January" | "February" | "March" | "April"
 	| "May" | "June" | 	"July" | "August"
 	| "September" | "October" | "November" | "December";
@@ -10,20 +11,88 @@ export declare type TimeUnit = "h" | "m" | "s" | "ms";
 
 export declare class CustomDate extends Date {
 	static months: Month[];
+
+	/**
+	 * Returns the suffix used in English for a given day.
+	 */
 	static getDaySuffix (number: number): EnglishDaySuffix;
+
+	/**
+	 * Compares two instances for their equality.
+	 */
 	static equals (from: CustomDate, to: CustomDate): boolean;
 
+	/**
+	 * Pads a number with specified number of zeroes.
+	 */
+	static zf (number: number): string;
+
+	/**
+	 * Creates the instance. Uses the same constructor as native Date does.
+	 */
 	constructor (...args: Array<number | string | Date>);
 
+	/**
+	 * Formats the instance into specified format.
+	 * For full documentation of the symbols, check here: {@link https://www.php.net/manual/en/datetime.format.php}
+	 */
 	format (formatString: string): Formatted;
+
+	/**
+	 * Determines if the date instance is valid, as in, its internal value is not `NaN`.
+	 */
 	isValid (): boolean;
+
+	/**
+	 * Syntactic sugar for format("j.n.Y")
+	 * @example "1.3.2021"
+	 */
 	simpleDate (): Formatted;
+
+	/**
+	 * Syntactic sugar for format("j.n.Y H:i:s")
+	 * @example "1.3.2021 08:07:01"
+	 */
+	simpleDateTime (): Formatted;
+
+	/**
+	 * Syntactic sugar for format("j.n.Y H:i:s.v")
+	 * @example "1.3.2021 08:07:01.055"
+	 */
 	fullDateTime (): Formatted;
+
+	/**
+	 * Syntactic sugar for format("Y-m-d")
+	 * @example "2021-03-01"
+	 */
 	sqlDate (): Formatted;
+
+	/**
+	 * Syntactic sugar for format("H:i:s.v")
+	 * @example "08:07:01.055"
+	 */
 	sqlTime (): Formatted;
+
+	/**
+	 * Syntactic sugar for format("Y-m-d H:i:s.v")
+	 * @example "2021-03-01 08:07:01.005"
+	 */
 	sqlDateTime (): Formatted;
+
+	/**
+	 * Sets the current timezone offset to the provided value.
+	 * The offset provided is in minutes
+	 */
 	setTimezoneOffset (offset: number): CustomDate;
+
+	/**
+	 * Sets the provided time units to zero.
+	 */
 	discardTimeUnits (...units: TimeUnit[]): CustomDate;
+
+	/**
+	 * Clones the current CustomDate. The new object is independent of the old one.
+	 */
 	clone (): CustomDate;
 
 	addYears (y: number): CustomDate;
@@ -34,7 +103,7 @@ export declare class CustomDate extends Date {
 	addSeconds (s: number): CustomDate;
 	addMilliseconds (ms: number): CustomDate;
 
-	get dayOfTheWeek (): number;
+	get dayOfTheWeek (): DayOfTheWeek;
 	get year (): number;
 	get month (): number;
 	get day (): number;
--- objects/date.js
+++ objects/date.js
@@ -17,11 +17,6 @@ module.exports = class CustomDate extends Date {
 		"December"
 	];
 
-	/**
-	 * Returns the suffix used in English for a given day.
-	 * @param {number} number Day of the month (1..31)
-	 * @returns {"st"|"nd"|"rd"|"th"}
-	 */
 	static getDaySuffix (number) {
 		if (typeof number !== "number" || Math.trunc(number) !== number) {
 			throw new Error("Input must be an integer");
@@ -43,23 +38,10 @@ module.exports = class CustomDate extends Date {
 		}
 	}
 
-	/**
-	 * Pads a number with specified number of zeroes.
-	 * @private
-	 * @param {number} number
-	 * @param {number} padding
-	 * @returns {string}
-	 */
 	static zf (number, padding) {
 		return ("0".repeat(padding) + number).slice(-padding);
 	}
 
-	/**
-	 * Compares two instances for their equality
-	 * @param {sb.Date} from
-	 * @param {sb.Date} to
-	 * @returns {boolean}
-	 */
 	static equals (from, to) {
 		const fromValue = from?.valueOf();
 		const toValue = to?.valueOf();
@@ -73,10 +55,6 @@ module.exports = class CustomDate extends Date {
 		return (fromValue === toValue);
 	}
 
-	/**
-	 * Creates the instance. Uses the same constructor as native Date does.
-	 * @param {*} args
-	 */
 	constructor (...args) {
 		if (args.length > 1 && args.every(i => typeof i === "number")) {
 			// Subtract one from the month parameter, because of how stupid the JS Date constructor does it.
@@ -86,11 +64,6 @@ module.exports = class CustomDate extends Date {
 		super(...args);
 	}
 
-	/**
-	 * Formats the instance into specified format.
-	 * @param {string} formatString
-	 * @returns {string}
-	 */
 	format (formatString) {
 		const year = this.year;
 		const month = this.month;
@@ -158,60 +131,33 @@ module.exports = class CustomDate extends Date {
 		return value;
 	}
 
-	/**
-	 * Determines if the date instance is valid, as in its internal value is not `NaN`.
-	 * @returns {boolean}
-	 */
 	isValid () {
 		return (Number.isNaN(this.valueOf()) === false);
 	}
 
-	/**
-	 * @returns {string} For example 1.3.2021
-	 */
 	simpleDate () {
 		return this.format("j.n.Y");
 	}
 
-	/**
-	 * @returns {string} For example 1.3.2021 08:07:01
-	 */
 	simpleDateTime () {
 		return this.format("j.n.Y H:i:s");
 	}
 
-	/**
-	 * @returns {string} For example 1.3.2021 08:07:01.055
-	 */
 	fullDateTime () {
 		return this.format("j.n.Y H:i:s.v");
 	}
 
-	/**
-	 * @returns {string} For example 2021-03-01
-	 */
 	sqlDate () {
 		return this.format("Y-m-d");
 	}
 
-	/**
-	 * @returns {string} For example 08:07:01.055
-	 */
 	sqlTime () {
 		return this.format("H:i:s.v");
 	}
-
-	/**
-	 * @returns {string} For example 2021-03-01 08:07:01.005
-	 */
 	sqlDateTime () {
 		return this.format("Y-m-d H:i:s.v");
 	}
 
-	/**
-	 * @param {number} offset in minutes
-	 * @returns {sb.Date}
-	 */
 	setTimezoneOffset (offset) {
 		offset = Number(offset);
 
@@ -226,11 +172,6 @@ module.exports = class CustomDate extends Date {
 		return this;
 	}
 
-	/**
-	 * Sets the provided time units to zero.
-	 * @param {..."h"|"m"|"s"|"ms"} units
-	 * @returns {sb.Date}
-	 */
 	discardTimeUnits (...units) {
 		for (const unit of units) {
 			switch (unit) {
@@ -253,80 +194,45 @@ module.exports = class CustomDate extends Date {
 		return this;
 	}
 
-	/**
-	 * Clones the current CustomDate. The new object is independent of the old one.
-	 * @returns {sb.Date}
-	 */
 	clone () {
 		return new this.constructor(this);
 	}
 
-	/**
-	 * @param {number} y Number of years to add.
-	 * @returns {sb.Date}
-	 */
 	addYears (y) {
 		this.year += y;
 		return this;
 	}
 
-	/**
-	 * @param {number} m Number of months to add.
-	 * @returns {sb.Date}
-	 */
 	addMonths (m) {
 		this.month += m;
 		return this;
 	}
 
-	/**
-	 * @param {number} d Number of days to add.
-	 * @returns {sb.Date}
-	 */
 	addDays (d) {
 		this.day += d;
 		return this;
 	}
 
-	/**
-	 * @param {number} h Number of hours to add.
-	 * @returns {sb.Date}
-	 */
 	addHours (h) {
 		this.hours += h;
 		return this;
 	}
 
-	/**
-	 * @param {number} m Number of minutes to add.
-	 * @returns {sb.Date}
-	 */
 	addMinutes (m) {
 		this.minutes += m;
 		return this;
 	}
 
-	/**
-	 * @param {number} s Number of seconds to add.
-	 * @returns {sb.Date}
-	 */
 	addSeconds (s) {
 		this.seconds += s;
 		return this;
 	}
 
-	/**
-	 * @param {number} ms Number of milliseconds to add.
-	 * @returns {sb.Date}
-	 */
 	addMilliseconds (ms) {
 		this.milliseconds += ms;
 		return this;
 	}
 
-	/**
-	 * @returns {"Sunday"|"Monday"|"Tuesday"|"Wednesday"|"Thursday"|"Friday"|"Saturday"} The day of the week in English.
-	 */
 	get dayOfTheWeek () {
 		switch (super.getDay()) {
 			case 0: return "Sunday";
@@ -341,100 +247,58 @@ module.exports = class CustomDate extends Date {
 		}
 	}
 
-	/**
-	 * @returns {number}
-	 */
 	get milliseconds () {
 		return super.getMilliseconds();
 	}
 
-	/**
-	 * @param {number} ms
-	 */
 	set milliseconds (ms) {
 		super.setMilliseconds(ms);
 	}
 
-	/**
-	 * @returns {number}
-	 */
 	get seconds () {
 		return super.getSeconds();
 	}
 
-	/**
-	 * @param {number} s
-	 */
 	set seconds (s) {
 		super.setSeconds(s);
 	}
 
-	/**
-	 * @returns {number}
-	 */
 	get minutes () {
 		return this.getMinutes();
 	}
 
-	/**
-	 * @param {number} m
-	 */
 	set minutes (m) {
 		super.setMinutes(m);
 	}
 
-	/**
-	 * @returns {number}
-	 */
 	get hours () {
 		return super.getHours();
 	}
 
-	/**
-	 * @param {number} h
-	 */
 	set hours (h) {
 		super.setHours(h);
 	}
 
-	/**
-	 * @returns {number}
-	 */
 	get day () {
 		return super.getDate();
 	}
 
-	/**
-	 * @param {number} d
-	 */
 	set day (d) {
 		super.setDate(d);
 	}
 
-	/**
-	 * @returns {number}
-	 */
 	get month () {
 		return super.getMonth() + 1;
 	}
 
-	/**
-	 * @param {number} m
-	 */
 	set month (m) {
 		super.setMonth(m - 1);
 	}
 
-	/**
-	 * @returns {number}
-	 */
 	get year () {
 		return super.getFullYear();
 	}
 
-	/**
-	 * @param {number} y
-	 */
 	set year (y) {
 		super.setFullYear(y);
 	}
