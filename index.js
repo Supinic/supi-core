2c509dcdc4e9d28fe73dca08cefd3687399b27c0
Supinic <supinic@protonmail.com>
2023-10-25 00:35:22 +0200

    name all `export * from` top level exports

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-export * from "./objects/date.js";
-export * from "./objects/error.js";
-export * from "./objects/promise.js";
+export * as Date from "./objects/date.js";
+export * as Error from "./objects/error.js";
+export * as Promise from "./objects/promise.js";
 
-export * from "./singletons/query/index.js";
-export * from "./singletons/cache.js";
-export * from "./singletons/metrics.js";
-export * from "./singletons/utils.js";
+export * as Query from "./singletons/query/index.js";
+export * as Cache from "./singletons/cache.js";
+export * as Metrics from "./singletons/metrics.js";
+export * as Utils from "./singletons/utils.js";
 
-export * from "./classes/config.js";
-export * from "./classes/got-proxy.js";
+export * as Config from "./classes/config.js";
+export * as Got from "./classes/got-proxy.js";
