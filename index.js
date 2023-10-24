f2a50ec824acba727e370d910d2965808f8c8ffe
Supinic <supinic@protonmail.com>
2023-10-25 00:53:15 +0200

    redo `export` syntax to remove `defaults` when importing from CJS

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-export * as Date from "./objects/date.js";
-export * as Error from "./objects/error.js";
-export * as Promise from "./objects/promise.js";
+import Date from "./objects/date.js";
+import Error from "./objects/error.js";
+import Promise from "./objects/promise.js";
 
-export * as Query from "./singletons/query/index.js";
-export * as Cache from "./singletons/cache.js";
-export * as Metrics from "./singletons/metrics.js";
-export * as Utils from "./singletons/utils.js";
+import Query from "./singletons/query/index.js";
+import Cache from "./singletons/cache.js";
+import Metrics from "./singletons/metrics.js";
+import Utils from "./singletons/utils.js";
 
-export * as Config from "./classes/config.js";
-export * as Got from "./classes/got-proxy.js";
+import Config from "./classes/config.js";
+import Got from "./classes/got-proxy.js";
+
+export {
+	Date,
+	Error,
+	Promise,
+
+	Config,
+	Got,
+
+	Query,
+	Cache,
+	Metrics,
+	Utils
+};
