6210cccc5173aff1203cf941144c04c28606a8ed
Supinic <supinic@protonmail.com>
2023-10-25 18:18:10 +0200

    standardized import/export order

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import Date from "./objects/date.js";
 import Error from "./objects/error.js";
 import Promise from "./objects/promise.js";
 
+import Config from "./classes/config.js";
+import Got from "./classes/got-proxy.js";
+
 import Query from "./singletons/query/index.js";
 import Cache from "./singletons/cache.js";
 import Metrics from "./singletons/metrics.js";
 import Utils from "./singletons/utils.js";
-
-import Config from "./classes/config.js";
-import Got from "./classes/got-proxy.js";
 
 export {
 	Date,
