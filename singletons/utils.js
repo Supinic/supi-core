0aa810e5a30b972e31470603a7437882e05adff3
Supinic <supinic@protonmail.com>
2023-11-01 19:47:56 +0100

    pruned Supibot-specific methods

--- singletons/utils.js
+++ singletons/utils.js
@@ -1,18 +1,10 @@
-// @todo refactor out all usages of sb.Got - this module must not rely on its "requirees" to properly import specific instances!
-
 import SupiDate from "../objects/date.js";
 import SupiError from "../objects/error.js";
 
 import { MersenneTwister19937, Random } from "random-js";
-import getInfoFfprobe from "ffprobe";
-import { roll } from "roll-dice";
-import { transliterate } from "transliteration";
 import { load as loadCheerio } from "cheerio";
-import { parse as parseChrono } from "chrono-node";
-import RSSParser from "rss-parser";
 import parseDuration from "duration-parser";
 
-const rssParser = new RSSParser();
 const randomizer = new Random(MersenneTwister19937.autoSeed());
 
 const byteUnits = {
@@ -416,154 +408,6 @@ export default class Utils {
 	}
 
 	/**
-	 * Fetches info about a provided Youtube video.
-	 * @param {string} query Search string
-	 * @param {string} key Youtube API key
-	 * @param {object} options = {} additional options
-	 * @param {number} [options.maxResults]
-	 * @param {boolean} [options.single]
-	 * @returns {Promise<string>}
-	 */
-	async searchYoutube (query, key, options = {}) {
-		const params = { ...options };
-		if (params.single) {
-			if (typeof params.maxResults !== "undefined") {
-				throw new SupiError({
-					message: "Cannot combine params maxResults and single"
-				});
-			}
-
-			params.maxResults = 1;
-		}
-
-		const { items } = await sb.Got("GenericAPI", {
-			url: `https://www.googleapis.com/youtube/v3/search`,
-			searchParams: {
-				key,
-				q: query,
-				type: "video",
-				part: "snippet",
-				maxResults: params.maxResults ?? "10",
-				sort: "relevance"
-			}
-		}).json();
-
-		const videoList = items
-			// This filtering shouldn't be necessary, but in some cases YouTube API returns playlists
-			// despite the `type` parameter being set to strictly return videos only.
-			.filter(i => i.id.kind === "youtube#video" && i.id.videoId)
-			.map(i => ({
-				ID: i.id.videoId,
-				title: i.snippet.title
-			}));
-
-		return (params.single)
-			? videoList[0] ?? null
-			: videoList;
-	}
-
-	/**
-	 * Fetches a Youtube playlist as an array of video IDs.
-	 * Optionally, limits the amount of videos fetched.
-	 * @param {Object} options
-	 * @params {string} options.key Google/Youtube API key
-	 * @params {string} options.playlistID Youtube playlist ID
-	 * @params {number} [options.perPage = 50] How many videos should be fetched per page.
-	 * @params {number} [options.limit] Limit the number of videos.
-	 * @params {string} [options.limitAction]
-	 * @returns {Promise<string[]>}
-	 */
-	async fetchYoutubePlaylist (options = {}) {
-		if (!options.key) {
-			throw new SupiError({
-				message: "No API key provided"
-			});
-		}
-		else if (!options.playlistID) {
-			throw new SupiError({
-				message: "No playlist ID provided"
-			});
-		}
-
-		const limit = options.limit ?? Infinity;
-		const baseParams = {
-			part: "snippet",
-			key: options.key,
-			maxResults: options.perPage ?? 50,
-			playlistId: options.playlistID
-		};
-
-		let pageToken = null;
-		const result = [];
-		do {
-			const searchParams = { ...baseParams };
-			if (pageToken) {
-				searchParams.pageToken = pageToken;
-			}
-
-			const { body: data, statusCode } = await sb.Got("GenericAPI", {
-				url: "https://www.googleapis.com/youtube/v3/playlistItems",
-				searchParams,
-				throwHttpErrors: false,
-				responseType: "json"
-			});
-
-			if (statusCode !== 200) {
-				return {
-					success: false,
-					reason: "not-found"
-				};
-			}
-
-			pageToken = data.nextPageToken;
-			result.push(...data.items.map(i => ({
-				ID: i.snippet.resourceId.videoId,
-				title: i.snippet.title,
-				channelTitle: i.snippet.channelTitle,
-				published: new SupiDate(i.snippet.publishedAt),
-				position: i.snippet.position
-			})));
-
-			if (options.limitAction === "trim" && result.length > limit) {
-				return result.slice(0, limit);
-			}
-			else if (data.pageInfo.totalResults > limit) {
-				if (options.limitAction === "error") {
-					throw new SupiError({
-						message: "Maximum amount of videos exceeded!",
-						args: {
-							limit,
-							amount: data.pageInfo.totalResults
-						}
-					});
-				}
-				else if (options.limitAction === "return") {
-					return {
-						success: false,
-						reason: "limit-exceeded",
-						limit,
-						amount: data.pageInfo.totalResults
-					};
-				}
-				else {
-					return {
-						success: true,
-						reason: "limit-exceeded",
-						amount: data.pageInfo.totalResults,
-						result,
-						limit
-					};
-				}
-			}
-		} while (pageToken);
-
-		return {
-			success: true,
-			result
-		};
-	}
-
-	/**
 	 * Pads a number with specified number of zeroes.
 	 * @param {number} number
 	 * @param {number} padding
@@ -571,73 +415,6 @@ export default class Utils {
 	 */
 	zf (number, padding) {
 		return ("0".repeat(padding) + number).slice(-padding);
-	}
-
-	/**
-	 * Returns string with the english ordinal assigned to it.
-	 * @param {number} number
-	 * @returns {string}
-	 */
-	formatEnglishOrdinal (number) {
-		number = Number(number);
-		switch (number) {
-			case 1:
-				return "1st";
-			case 2:
-				return "2nd";
-			case 3:
-				return "3rd";
-			default:
-				return "th";
-		}
-	}
-
-	/**
-	 * Returns Google Geo Data for given query
-	 * @param {string} key Google Geo API key
-	 * @param {string} query
-	 * @returns {Promise<Object>}
-	 */
-	async fetchGeoLocationData (key, query) {
-		const { results, status } = await sb.Got("GenericAPI", {
-			url: "https://maps.googleapis.com/maps/api/geocode/json",
-			searchParams: {
-				key,
-				address: query
-			}
-		}).json();
-
-		if (status !== "OK") {
-			return {
-				success: false,
-				cause: status
-			};
-		}
-
-		const {
-			address_components: components,
-			formatted_address: formatted,
-			place_id: placeID,
-			geometry: { location }
-		} = results[0];
-
-		const object = {};
-		for (const row of components) {
-			let { types, long_name: long } = row;
-			if (types.includes("political")) {
-				types = types.filter(i => i !== "political");
-				types[0] = types[0].replace(/_/g, "").replace("administrativearea", "");
-				object[types[0]] = long;
-			}
-		}
-
-		return {
-			success: true,
-			components: object,
-			placeID,
-			location,
-			formatted
-		};
 	}
 
 	/**
@@ -672,20 +449,6 @@ export default class Utils {
 		}
 
 		return number ?? null;
-	}
-
-	parseChrono (string, referenceDate = null, options = {}) {
-		const chronoData = parseChrono(string, referenceDate, options);
-		if (chronoData.length === 0) {
-			return null;
-		}
-
-		const [chrono] = chronoData;
-		return {
-			date: chrono.start.date(),
-			component: chrono.start,
-			text: chrono.text
-		};
 	}
 
 	convertCase (text, caseFrom, caseTo) {
@@ -754,15 +517,6 @@ export default class Utils {
 	}
 
 	/**
-	 * Transliterates non-latin text into latin characters.
-	 * @param {...string} args
-	 * @returns {string}
-	 */
-	transliterate (...args) {
-		return transliterate(...args);
-	}
-
-	/**
 	 * Splits an array into two, based on the condition function.
 	 * @param {Array} array
 	 * @param {Function} filter Takes three arguments: (item, index, array)
@@ -789,36 +543,9 @@ export default class Utils {
 		return [yes, no];
 	}
 
-	parseURL (stringURL) {
-		return require("url").parse(stringURL);
-	}
-
-	/**
-	 * Returns the URL's pathname + search params, if defined.
-	 * Returns null if it is in any way invalid.
-	 * @param {string} stringURL
-	 * @returns {null|string}
-	 */
-	getPathFromURL (stringURL) {
-		if (!stringURL) {
-			return null;
-		}
-
-		const { URL } = require("url");
-		let url;
-		try {
-			url = new URL(stringURL);
-		}
-		catch {
-			return null;
-		}
-
-		const path = (url.pathname ?? "").replace(/^\//, "");
-		return `${path}${url.search}`;
-	}
-
 	/**
 	 * Utils wrapper for the cheerio module.
+	 * @todo remove from sb.Utils completely, and use without wrapping in projects as necessary
 	 * @param {string} html
 	 * @returns {*} CheerioAPI
 	 */
@@ -903,46 +630,6 @@ export default class Utils {
 	}
 
 	/**
-	 * Levenshtein distance of two strings.
-	 * @param {string} from
-	 * @param {string} target
-	 * @returns {number}
-	 */
-	levenshteinDistance (from, target) {
-		if (from.length === 0) {
-			return target.length;
-		}
-		if (target.length === 0) {
-			return from.length;
-		}
-
-		const matrix = [];
-		for (let i = 0; i <= target.length; i++) {
-			matrix[i] = [i];
-		}
-
-		for (let i = 0; i <= from.length; i++) {
-			matrix[0][i] = i;
-		}
-
-		for (let i = 1; i <= target.length; i++) {
-			for (let j = 1; j <= from.length; j++) {
-				if (target[i - 1] === from[j - 1]) {
-					matrix[i][j] = matrix[i - 1][j - 1];
-				}
-				else {
-					matrix[i][j] = Math.min(
-						matrix[i - 1][j - 1] + 1,
-						Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1)
-					);
-				}
-			}
-		}
-
-		return matrix[target.length][from.length];
-	}
-
-	/**
 	 * Jaro-Winkler similarity of two strings.
 	 * @param {string} from
 	 * @param {string} target
@@ -1018,14 +705,6 @@ export default class Utils {
 	}
 
 	/**
-	 * @typedef {Object} ClosestStringDescriptor
-	 * @property {string} string
-	 * @property {string} original
-	 * @property {number} index
-	 * @property {number} score
-	 * @property {boolean} includes
-	 */
-	/**
 	 * Returns the best fit for given string, based on Levenshtein distance.
 	 * @param {string} from
 	 * @param {string[]} originalTargets
@@ -1097,34 +776,6 @@ export default class Utils {
 	groupDigits (number, separator = " ") {
 		const local = new Intl.NumberFormat().format(number);
 		return local.replace(/,/g, separator);
-	}
-
-	/**
-	 * Parses an RSS string into JS object format.
-	 * @param {string} xml
-	 * @returns {Promise<RSSParserResult>}
-	 */
-	async parseRSS (xml) {
-		return await rssParser.parseString(xml);
-	}
-
-	/**
-	 * Uses ffmpeg to probe a file for its media file data.
-	 * @param {string} link
-	 * @returns {Promise<{duration: number, bitrate: number}|null>}
-	 */
-	async getMediaFileData (link) {
-		try {
-			const path = sb.Config.get("FFMPEG_PATH");
-			const { streams } = await getInfoFfprobe(link, { path });
-			return {
-				duration: Number(streams[0].duration),
-				bitrate: Number(streams[0].bit_rate)
-			};
-		}
-		catch {
-			return null;
-		}
 	}
 
 	/**
@@ -1204,319 +855,6 @@ export default class Utils {
 	}
 
 	/**
-	 * Evaluates an expression in standard dice notation form
-	 * @param {string} input
-	 * @param {number} limit max number of rolls in a single evaluation
-	 * @returns {number|null}
-	 */
-	evalDiceRoll (input, limit) {
-		const seed = this.random(0, 1e12);
-		const resultLimit = limit ?? Number.MAX_SAFE_INTEGER;
-		try {
-			return Number(roll(input, BigInt(seed), BigInt(resultLimit)));
-		}
-		catch {
-			return null;
-		}
-	}
-
-	/**
-	 * Uploads a file to {@link https://imgur.com}
-	 * @param {Buffer} fileData
-	 * @param {string} [linkName]
-	 * @param {Object} [options]
-	 * @param {string} [options.type]
-	 * @returns {Promise<FileUploadResult>}
-	 */
-	async uploadToImgur (fileData, linkName = "random", options = {}) {
-		const formData = new sb.Got.FormData();
-		formData.append("image", fileData, linkName); // !!! FILE NAME MUST BE SET, OR THE API NEVER RESPONDS !!!
-
-		const { type = "image" } = options;
-		const endpoint = (type === "image")
-			? "image"
-			: "upload";
-
-		const { statusCode, body } = await sb.Got("GenericAPI", {
-			url: `https://api.imgur.com/3/${endpoint}`,
-			responseType: "json",
-			method: "POST",
-			throwHttpErrors: false,
-			headers: {
-				...formData.getHeaders(),
-				Authorization: "Client-ID c898c0bb848ca39"
-			},
-			body: formData.getBuffer(),
-			retry: {
-				limit: 0
-			},
-			timeout: {
-				request: 10_000
-			}
-		});
-
-		// Weird edge case with Imgur when uploading .webm or .mkv files will leave a "." at the end of the link
-		let link = body.data?.link ?? null;
-		if (typeof link === "string" && link.endsWith(".")) {
-			link = `${link}mp4`;
-		}
-
-		return {
-			statusCode,
-			link
-		};
-	}
-
-	/**
-	 * Uploads a file to {@link https://i.nuuls.com}
-	 * @param {Buffer} fileData
-	 * @param {string} [fileName] custom filename, used for managing extensions
-	 * @returns {Promise<FileUploadResult>}
-	 */
-	async uploadToNuuls (fileData, fileName = "file.jpg") {
-		const form = new sb.Got.FormData();
-		form.append("attachment", fileData, fileName);
-
-		const response = await sb.Got("GenericAPI", {
-			method: "POST",
-			throwHttpErrors: false,
-			url: "https://i.nuuls.com/upload",
-			responseType: "text",
-			headers: {
-				...form.getHeaders()
-			},
-			body: form.getBuffer(),
-			retry: {
-				limit: 0
-			},
-			timeout: {
-				request: 10_000
-			}
-		});
-
-		return {
-			statusCode: response.statusCode,
-			link: response.body ?? null
-		};
-	}
-
-	/**
-	 * Checks an image provided via URL for its NSFW score.
-	 * @param {string} link
-	 * @returns {Promise<NSFWDetectionResult>}
-	 */
-	async checkPictureNSFW (link) {
-		const { statusCode, body: data } = await sb.Got("GenericAPI", {
-			method: "POST",
-			responseType: "json",
-			throwHttpErrors: false,
-			url: "https://api.deepai.org/api/nsfw-detector",
-			headers: {
-				"Api-Key": sb.Config.get("API_DEEP_AI")
-			},
-			form: {
-				image: link
-			}
-		});
-
-		if (data.output?.detections) {
-			for (const item of data.output.detections) {
-				item.confidence = Number(item.confidence);
-			}
-		}
-
-		return {
-			statusCode,
-			data: {
-				id: data.id ?? null,
-				score: data.output?.nsfw_score ?? null,
-				detections: data.output?.detections ?? null
-			}
-		};
-	}
-
-	/**
-	 * Fetches time data for given GPS coordinates and timestamp
-	 * @param {Object} data
-	 * @param {string} data.key
-	 * @param {Object} data.coordinates
-	 * @param {string} data.coordinates.lng
-	 * @param {string} data.coordinates.lat
-	 * @param {Date|SupiDate|number} data.date = new SupiDate()
-	 * @returns {Promise<{body: Object, statusCode: number}>}
-	 */
-	async fetchTimeData (data = {}) {
-		const {
-			coordinates,
-			date = new SupiDate(),
-			key
-		} = data;
-
-		const response = await sb.Got("Google", {
-			url: "timezone/json",
-			searchParams: {
-				timestamp: Math.trunc(date.valueOf() / 1000),
-				location: `${coordinates.lat},${coordinates.lng}`,
-				key
-			}
-		});
-
-		return {
-			statusCode: response.statusCode,
-			body: response.body
-		};
-	}
-
-	/**
-	 * Processes a list of resulting images from Artflow.ai and incorporates them into the database
-	 * @param {Array} items
-	 * @returns {Promise<*[]>}
-	 */
-	async processArtflowData (items) {
-		const result = [];
-
-		for (const item of items) {
-			if (!item.filename) {
-				result.push({
-					saved: false,
-					reason: "missing-filename"
-				});
-
-				continue;
-			}
-
-			const row = await sb.Query.getRow("data", "Artflow_Image");
-			await row.load(item.filename, true);
-			if (row.loaded) { // Image already exists in the database
-				if (item.userDataID) {
-					row.values.User_Alias = item.userDataID;
-					await row.save({ skipLoad: true });
-				}
-
-				result.push({
-					saved: false,
-					reason: "already-exists",
-					link: row.values.Upload_Link
-				});
-
-				continue;
-			}
-
-			if (item.status === "Queuing") { // Image currently being processed, skip
-				result.push({
-					saved: false,
-					reason: "in-queue"
-				});
-
-				continue;
-			}
-
-			let imageURL = `https://artflowbucket.s3.amazonaws.com/generated/${item.index}.webp`;
-			const indexResponse = await sb.Got("FakeAgent", {
-				method: "HEAD",
-				url: imageURL,
-				throwHttpErrors: false,
-				responseType: "text"
-			});
-
-			if (indexResponse.statusCode !== 200) {
-				imageURL = `https://artflowbucket-new.s3.amazonaws.com/generated/${item.filename}.webp`;
-
-				const filenameResponse = await sb.Got("FakeAgent", {
-					method: "HEAD",
-					url: imageURL,
-					throwHttpErrors: false,
-					responseType: "text"
-				});
-
-				if (filenameResponse.statusCode !== 200) {
-					imageURL = null;
-				}
-			}
-
-			if (!imageURL) { // File does not exist anymore
-				result.push({
-					saved: false,
-					reason: "image-not-found"
-				});
-
-				continue;
-			}
-
-			const formData = new sb.Got.FormData();
-			formData.append("reqtype", "urlupload");
-			formData.append("url", imageURL);
-
-			let uploadResponse;
-			try {
-				uploadResponse = await sb.Got("GenericAPI", {
-					url: "https://catbox.moe/user/api.php",
-					method: "POST",
-					throwHttpErrors: false,
-					headers: {
-						...formData.getHeaders()
-					},
-					body: formData.getBuffer(),
-					retry: {
-						limit: 0
-					},
-					timeout: {
-						request: 10_000
-					}
-				});
-			}
-			catch (e) {
-				if (e instanceof sb.Got.TimeoutError) {
-					result.push({
-						saved: false,
-						reason: "upload-timed-out"
-					});
-
-					continue;
-				}
-				else {
-					throw e;
-				}
-			}
-
-			if (uploadResponse.statusCode !== 200) { // Upload failed
-				result.push({
-					saved: false,
-					reason: "upload-failed",
-					data: {
-						statusCode: uploadResponse.statusCode
-					}
-				});
-
-				continue;
-			}
-
-			result.push({
-				saved: true,
-				link: uploadResponse.body,
-				id: item.index,
-				filename: item.filename
-			});
-
-			row.setValues({
-				Filename: item.filename,
-				ID: item.index,
-				User_ID: item.userID ?? null,
-				User_Alias: item.userDataID ?? null,
-				Prompt: item.text_prompt ?? null,
-				Upload_Link: uploadResponse.body
-			});
-
-			await row.save({
-				ignore: true,
-				skipLoad: true
-			});
-		}
-
-		return result;
-	}
-
-	/**
 	 * Prepares a regex string by escaping all special regex characters.
 	 * @param {string} string
 	 * @returns {string}
@@ -1531,11 +869,11 @@ export default class Utils {
 
 	/**
 	 * Creates a regular expression based on a provided string input.
-	 * @param {string} value
+	 * @param {string} input
 	 * @returns {RegExp|null} Returns `null` if the regex creation fails with an error
 	 */
-	parseRegExp (value) {
-		const string = value.replace(/^\/|\/$/g, "");
+	parseRegExp (input) {
+		const string = input.replace(/^\/|\/$/g, "");
 
 		// find last possible forward slash that is not escaped with a backslash
 		// this determines the forceful end of a regex, which is then followed by flag characters
@@ -1569,49 +907,10 @@ export default class Utils {
 }
 
 /**
- * @typedef {Object} NSFWDetectionResult
- * @property {number} statusCode
- * @property {Object} data
- * @property {string} data.id
- * @property {number} data.score
- * @property {NSFWDetectionItem[]} data.detections
- */
-
-/**
- * @typedef {Object} NSFWDetectionItem
- * @property {number[]} bounding_box Array of four numbers determining the bounding box coordinates
- * @property {number} confidence
- * @property {string} name
- */
-
-/**
- * @typedef {Object} FileUploadResult
- * @property {number} statusCode
- * @property {string} link
- */
-
-/**
- * @typedef {Object} RSSParserResult
- * @property {string} description
- * @property {Object} image
- * @property {string} image.link
- * @property {string} image.title
- * @property {string} image.url
- * @property {RSSArticle[]} items
- * @property {string} language
- * @property {string} link
- * @property {string} title
- */
-
-/**
- * @typedef {Object} RSSArticle
- * @property {string} author
- * @property {string[]} categories
- * @property {string} content
- * @property {string} contentSnippet
- * @property {string} creator
- * @property {string} isoDate
- * @property {string} link
- * @property {string} pubDate
- * @property {string} title
+ * @typedef {Object} ClosestStringDescriptor
+ * @property {string} string
+ * @property {string} original
+ * @property {number} index
+ * @property {number} score
+ * @property {boolean} includes
  */
