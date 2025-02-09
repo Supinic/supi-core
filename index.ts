6959558da15848eea66fb1b40fe7eb8b804941b6
supinic <supinic@protonmail.com>
2025-02-09 11:13:03 +0100

    re-do exports

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
-export * from "./objects/date.js";
-export * from "./objects/error.js";
-export * from "./objects/promise.js";
-export * from "./classes/got-proxy.js";
-export * from "./singletons/query/index.js";
-export * from "./singletons/cache.js";
-export * from "./singletons/metrics.js";
-export * from "./singletons/utils.js";
+export { SupiError, GenericRequestError } from "./objects/error.js";
+
+import Date from "./objects/date.js";
+import Promise from "./objects/promise.js";
+import Got from "./classes/got-proxy.js";
+import Query from "./singletons/query/index.js";
+import Cache from "./singletons/cache.js";
+import Metrics from "./singletons/metrics.js";
+import Utils from "./singletons/utils.js";
+
+export {
+	Date,
+	Promise,
+	Got,
+	Query,
+	Cache,
+	Metrics,
+	Utils
+};
