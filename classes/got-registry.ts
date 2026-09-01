8ff02c03c14ffec35b999b945717a3d4cf2f2610
supinic <supinic@protonmail.com>
2026-09-01 23:52:54 +0200

    type adjustments, generics, resolveBodyOnly

--- classes/got-registry.ts
+++ classes/got-registry.ts
@@ -1,28 +1,32 @@
-import got, { type Got, type OptionsInit as GotOptions, type RequestPromise, type Response as GotResponse, RequestError } from "got";
-export type { Response as GotResponse } from "got";
+import got, { type Got, type OptionsInit as GotOptions, type RequestPromise, type Response, RequestError } from "got";
+import type { JSONifiable } from "../singletons/query/index.js";
 
-export type GotRequestOptions = Omit<GotOptions, "url"> & {
+export type GotResponse<T = unknown> = Response<T>;
+export type GotRequestOptions = Omit<GotOptions, "url" | "resolveBodyOnly"> & {
 	url: string | URL;
+	resolveBodyOnly?: false; // prohibit returning body only
 };
 export type GqlRequestOptions = Omit<GotRequestOptions, "body" | "form" | "json" | "method" | "responseType" | "resolveBodyOnly"> & {
 	query: string;
 	instance?: string;
-	variables?: Record<string, unknown>;
+	variables?: Record<string, JSONifiable>;
 	token?: string;
 };
 
+type GotRegistryOptions = Omit<GotOptions, "url" | "resolveBodyOnly"> & {
+	resolveBodyOnly?: false; // prohibit returning body only
+};
 export type GotRegistryInstanceDefinition = {
 	name: string;
 	parent?: string;
-	options: GotOptions | (() => GotOptions);
+	options: GotRegistryOptions | (() => GotRegistryOptions);
 };
 
-type WrappedGot = (options: GotRequestOptions) => ReturnType<Got>;
+type WrappedGot = (options: GotRequestOptions) => RequestPromise<GotResponse>;
 type RegisteredGot = {
 	raw: Got;
 	request: WrappedGot;
 };
-
 
 const wrap = (instance: Got): WrappedGot => ({ url, ...options }) => instance(url, options);
 
@@ -31,7 +35,7 @@ export const isGotRequestError = (input: unknown): input is RequestError => (inp
 export class GotRegistry {
 	private readonly instances = new Map<string, RegisteredGot>();
 
-	get (name: string): WrappedGot {
+	public get (name: string): WrappedGot {
 		const instance = this.instances.get(name);
 		if (!instance) {
 			throw new Error(`Unknown Got instance "${name}"`);
@@ -40,7 +44,7 @@ export class GotRegistry {
 		return instance.request;
 	}
 
-	getRaw (name: string): Got {
+	private getRaw (name: string): Got {
 		const instance = this.instances.get(name);
 		if (!instance) {
 			throw new Error(`Unknown Got instance "${name}"`);
@@ -49,27 +53,33 @@ export class GotRegistry {
 		return instance.raw;
 	}
 
-	gql (options: GqlRequestOptions): RequestPromise<GotResponse<unknown>> {
+	public gql (options: GqlRequestOptions): RequestPromise<GotResponse<unknown>> {
 		const { url, query, variables, token, headers, instance, ...rest } = options;
 		const target = (instance) ? this.getRaw(instance) : got;
 
-		const json = (variables) ? { query, variables } : { query };
 		return target(url, {
 			...rest,
 			method: "POST",
 			responseType: "json",
+			resolveBodyOnly: false,
 			headers: (token)
-				? { ...headers, Authorization: `Bearer ${token}` }
+				? { ...headers, authorization: `Bearer ${token}` }
 				: headers,
-			json
+			json: (variables)
+				? { query, variables }
+				: { query }
 		})
 	}
 
-	add (definition: GotRegistryInstanceDefinition): void {
+	public add (definition: GotRegistryInstanceDefinition): void {
+		if (this.instances.has(definition.name)) {
+			throw new Error(`Got instance ${definition.name} already exists`);
+		}
+
 		const options = (typeof definition.options === "function") ? definition.options() : definition.options;
 		const parent = (definition.parent) ? this.getRaw(definition.parent) : got;
-
 		const instance = parent.extend(options);
+
 		this.instances.set(definition.name, {
 			raw: instance,
 			request: wrap(instance)
